5c0c87622abf2df66ca256e69151edc4bf12deff
Morgan Zero <57449785+morganzero@users.noreply.github.com>
2020-11-08 01:43:32 +0100

    Update dietplex.sh

--- dietplex.sh
+++ dietplex.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+echo "Calculating weight..."
+sleep 5
+echo "Go grab something to drink while you wait."
+sleep 2
+echo "...maybe something with zero sugar"
+sleep 3
 plexdir="/opt/appdata/plex/database/Library/Application Support/Plex Media Server/"
 plexsize=$(du -hs "$plexdir" | cut -f1)
 echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
@@ -11,6 +17,12 @@ echo "Metadata           :  $metadatasize"
 transcoderdir="/opt/appdata/plex/database/Library/Application Support/Plex Media Server/Cache/PhotoTranscoder/"
 transcodersize=$(du -hs "$transcoderdir" | cut -f1)
 echo "PhotoTranscoder    :  $transcodersize"
+anidbdir="/opt/appdata/plex/database/Library/Application Support/Plex Media Server/Plug-in Support/Data/com.plexapp.agents.hama/DataItems/"
+if [ -d "$anidbdir" ]
+then
+anidbsize=$(du -hs "$anidbdir" | cut -f1)
+echo "HAMA (Anidb)       :  $anidbsize"
+fi
 echo
 echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
 echo
@@ -43,7 +55,7 @@ echo
 echo
 sleep 1
 echo
-echo "🏆 Diet succeeded! Weightloss: $metadatasize"
+echo "🏆 Diet succeeded! Weight reached: $metadatasize"
 echo
 sleep 1
 echo ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
@@ -57,7 +69,7 @@ while true; do
 echo
 echo
 echo ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-    read -r -p "↘️  Type Y/N | Press [ENTER]:" thisisit
+    read -r -p "↘️  Make your choice | Type Y/N and press [ENTER]:  " thisisit
 echo
     case $thisisit in
         [yY]* ) rm -f "$transcoderdir{*,.*}"; break;;
@@ -68,6 +80,29 @@ echo
     esac
 done
 sleep 2
+anidbdir="/opt/appdata/plex/database/Library/Application Support/Plex Media Server/Plug-in Support/Data/com.plexapp.agents.hama/DataItems/"
+if [ -d "$anidbdir" ]
+then
+anidbsize=$(du -hs "$anidbdir" | cut -f1)
+while true; do
+echo ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+echo "HAMA is using: $anidbsize"
+echo ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+echo
+  echo " [Y]  :   Yeah, slim it down!"
+  echo " [N]  :   ON'T TOUCH MY HAMA!"
+echo
+echo
+echo ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+  read -r -p "↘️  Make your choice | Type Y/N and press [ENTER]:  " thisisit
+echo
+    case $thisisit in
+        [yY]* ) find "$anidbdir" -mindepth 1 -exec rm -r {} \; && echo "Done!"; break;;
+        [nN]* ) break;;
+        * ) echo "🔪 Don't be a retard, please chose either Y or N";;
+    esac
+done
+fi
 echo ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 echo
 echo "Extreme diet finished. "
@@ -77,6 +112,6 @@ echo "Cleaning up ...🧹"
 sleep 1
 echo
 echo ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-echo "✅ New Plex Size is: $plexsize"
+echo "🍻 New Plex Size is: $plexsize"
 echo ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 echo
